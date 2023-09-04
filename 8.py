19807f79f99524759f396c56a5a9cf536d293737
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-09-04 15:57:39 +0200

    Update 8.py

--- 8.py
+++ 8.py
@@ -16,11 +16,11 @@ def halfeight():
  print(arlo.go_diff(80, 30, 1, 1))
  
  # Wait a bit while robot moves forward
- sleep(9)
+ sleep(6)
  
  # turn 
  print(arlo.go_diff(30, 80, 1, 1))
- sleep(9)
+ sleep(6)
  
 
 def eight():
