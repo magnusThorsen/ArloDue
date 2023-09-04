53fd4c8d7e5189338b163f9381564e7b75417eae
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-09-04 15:56:06 +0200

    Update 8.py

--- 8.py
+++ 8.py
@@ -13,14 +13,14 @@ leftSpeed = 64
 rightSpeed = 66
 
 def halfeight():
- print(arlo.go_diff(60, 16, 1, 1))
+ print(arlo.go_diff(80, 30, 1, 1))
  
  # Wait a bit while robot moves forward
  sleep(9)
  
  # turn 
- print(arlo.go_diff(30, 16, 1, 1))
- sleep()
+ print(arlo.go_diff(30, 80, 1, 1))
+ sleep(9)
  
 
 def eight():
