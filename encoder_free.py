9f2d93042d59c8fac5377e2f79222d949e62f6fc
KasperFrahm <gjr449@alumni.ku.dk>
2023-09-06 15:04:55 +0200

    Update encoder_free.py

--- encoder_free.py
+++ encoder_free.py
@@ -20,50 +20,20 @@ print("Back sensor = ", arlo.read_back_ping_sensor())
 print("Right sensor = ", arlo.read_right_ping_sensor())
 
 # request to read Left sonar ping sensor
-print("Left sensor = ", arlo.read_left_ping_sensor())
+print("Left sensor = ", arlo.read_left_ping_sensor())  
+    
+print(arlo.go_diff(64, 64, 1, 1))
+isDriving = True
+while (isDriving): # or some other form of loop
+    frontSensor = arlo.read_front_ping_sensor()
+    backSensor = arlo.read_back_ping_sensor()
+    rightSensor = arlo.read_right_ping_sensor()
+    leftSensor = arlo.read_left_ping_sensor()
 
-"""""
-# send a go_diff command to drive forward in a curve turning right
-leftSpeed = 64
-rightSpeed = 64
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-
-# Wait a bit while robot moves forward
-sleep(3)
-
-
-# send a go_diff command to drive forward in a curve turning right
-leftSpeed = 64
-rightSpeed = 64
-print(arlo.go_diff(leftSpeed, rightSpeed, 0, 0))
-
-# Wait a bit while robot moves forward
-sleep(3)
-
-
-
-# send a go_diff command to drive forward in a curve turning right
-leftSpeed = 64
-rightSpeed = 32
-print(arlo.go_diff(leftSpeed, rightSpeed, 1, 1))
-
-# Wait a bit while robot moves forward
-sleep(3)
-
-# send a stop command
-print(arlo.stop())
-
-sleep(0.05)
-
-
-# send a go_diff command to drive backwards the same way we came from
-print(arlo.go_diff(leftSpeed, rightSpeed, 0, 0))
-
-# Wait a bit while robot moves backwards
-sleep(3)
-
-# send a stop command
-print(arlo.stop())
-"""
+    if frontSensor < 100:
+        print(arlo.go_diff(64, 64, 0, 0))
+        sleep(0.5)
+        isDriving = False
+        
 
 print("Finished")
