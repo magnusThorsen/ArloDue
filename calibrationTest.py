baab290355586178e16873c12e46775c64fbba4f
KasperFrahm <gjr449@alumni.ku.dk>
2023-09-13 11:09:20 +0200

    Create calibrationTest.py

---
+++ calibrationTest.py
@@ -0,0 +1,25 @@
+import time
+from time import sleep
+
+import robot
+
+# Create a robot object and initialize
+arlo = robot.Robot()
+
+
+sleep(1)
+
+
+# request to read Front sonar ping sensor
+print("Front sensor = ", arlo.read_front_ping_sensor())
+
+# request to read Back sonar ping sensor
+print("Back sensor = ", arlo.read_back_ping_sensor())
+
+# request to read Right sonar ping sensor
+print("Right sensor = ", arlo.read_right_ping_sensor())
+
+# request to read Left sonar ping sensor
+print("Left sensor = ", arlo.read_left_ping_sensor())
+
+print("Finished")
