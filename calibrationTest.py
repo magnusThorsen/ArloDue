b71a1a64924ff9b6f3e47a55dd64ca8761ac03d9
KasperFrahm <gjr449@alumni.ku.dk>
2023-09-13 11:12:28 +0200

    Update calibrationTest.py

--- calibrationTest.py
+++ calibrationTest.py
@@ -9,17 +9,17 @@ arlo = robot.Robot()
 
 sleep(1)
 
+while(True):
+    # request to read Front sonar ping sensor
+    print("Front sensor = ", arlo.read_front_ping_sensor())
 
-# request to read Front sonar ping sensor
-print("Front sensor = ", arlo.read_front_ping_sensor())
+    # request to read Back sonar ping sensor
+    print("Back sensor = ", arlo.read_back_ping_sensor())
 
-# request to read Back sonar ping sensor
-print("Back sensor = ", arlo.read_back_ping_sensor())
+    # request to read Right sonar ping sensor
+    print("Right sensor = ", arlo.read_right_ping_sensor())
 
-# request to read Right sonar ping sensor
-print("Right sensor = ", arlo.read_right_ping_sensor())
-
-# request to read Left sonar ping sensor
-print("Left sensor = ", arlo.read_left_ping_sensor())
+    # request to read Left sonar ping sensor
+    print("Left sensor = ", arlo.read_left_ping_sensor())
 
 print("Finished")
