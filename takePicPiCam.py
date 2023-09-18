1b646c738536fc4d028c544166f113c3da498f15
NikSchaltz <nikolaj.schaltz@gmail.com>
2023-09-18 15:05:28 +0200

    Update takePicPiCam.py

--- takePicPiCam.py
+++ takePicPiCam.py
@@ -39,17 +39,17 @@ WIN_RF = "Example 1"
 cv2.namedWindow(WIN_RF)
 cv2.moveWindow(WIN_RF, 100, 100)
 
-cnt = 120
+cnt = 100
 while cv2.waitKey(4) == -1: # Wait for a key pressed event
     image = cam.capture_array("main")
     # Show frames
     cv2.imshow(WIN_RF, image)
     if cv2.waitKey(4) == 32: #takes picture when pressing space
-        cnt += 20
         print("pressed space")
         path = '/Pictures'
         filename = 'Pictures/' + str(cnt) + '.jpg'
         cv2.imwrite(filename, image)
+        cnt += 20
 
 
 # Finished successfully
