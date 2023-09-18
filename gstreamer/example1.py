d3503908156156245ff6a218475e1f3b4524898c
NikSchaltz <nikolaj.schaltz@gmail.com>
2023-09-18 13:52:06 +0200

    Update example1.py

--- gstreamer/example1.py
+++ gstreamer/example1.py
@@ -4,7 +4,7 @@
 import cv2 # Import the OpenCV library
 
 
-def gstreamer_pipeline(capture_width=1024, capture_height=720, framerate=30):
+def gstreamer_pipeline(capture_width=800, capture_height=600, framerate=30):
     """Utility function for setting parameters for the gstreamer camera pipeline"""
     return (
         "libcamerasrc !"
