b6f180d6680dcb93974d5f572bae97e724417990
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-09-25 15:24:26 +0200

    Update detectLandmark.py

--- detectLandmark.py
+++ detectLandmark.py
@@ -67,7 +67,7 @@ def drive30CM():
 
 def drive(distance):
     left_speed = 30
-    right_speed = 33.5
+    right_speed = 33.75
 
     # Calculate time based on distance and wheel speeds
     #average_speed = (left_speed + right_speed) / 2
