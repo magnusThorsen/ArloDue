781f1e46213bfcfffb59ded58c13573929a59b19
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-09-25 15:00:41 +0200

    Update detectLandmark.py

--- detectLandmark.py
+++ detectLandmark.py
@@ -71,7 +71,7 @@ def drive(distance):
 
     # Calculate time based on distance and wheel speeds
     #average_speed = (left_speed + right_speed) / 2
-    time = distance / 28
+    time = distance / 25
     print("time",time)
     print("distance",distance)
 
