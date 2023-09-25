9023519a075aab5586d696accf3f26be09dde810
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-09-25 15:26:14 +0200

    Update detectLandmark.py

--- detectLandmark.py
+++ detectLandmark.py
@@ -143,21 +143,18 @@ while val: # Wait for a key pressed event (cv2.waitKey(4) == -1)
     # print go diff 
     detected, rvecs, tvecs = searchAndshow()
     if not detected: 
-        drive(200)
-        """ print(arlo.go_diff(32, 35, 0, 1))
+        print(arlo.go_diff(32, 35, 0, 1))
         sleep(0.15)
         print(arlo.stop())
-        sleep(0.1) """
-        val = False
+        sleep(0.1)
     else: 
-        drive(200)
-        """ print(arlo.stop())
+        print(arlo.stop())
         print("rvecs direction: ",rvecs/np.linalg.norm(rvecs))
         print("rvecs: ",np.linalg.norm(rvecs))
         print("tvecs: ",np.linalg.norm(tvecs) / 14.086079 )
         print("turning left.. ")
         turnLeft(np.linalg.norm(rvecs)*3)
-        drive(100) #np.linalg.norm(tvecs)/14.086079 """
+        drive(np.linalg.norm(tvecs)/14.086079) #
         val = False
         
 
