2522ccc8ff364fa15dc36b198f5612a121f039be
jakobdue <jakob.s.due@gmail.com>
2023-10-04 13:59:07 +0200

    Update rrt.py

--- rrt.py
+++ rrt.py
@@ -199,9 +199,9 @@ def main():
         goal=[0, 60],
         robot_model=robot,
         map=map,
-        expand_dis=2,
+        expand_dis=5,
         path_resolution=path_res,
-        goal_sample_rate=50,
+        goal_sample_rate=25,
         )
     
     show_animation = True
