049c9f321cdf9574470e7e91eb158ae46cbbd94e
KasperFrahm <gjr449@alumni.ku.dk>
2023-10-04 14:41:52 +0200

    Update PathPlanGo.py

--- PathPlanGo.py
+++ PathPlanGo.py
@@ -14,36 +14,6 @@ np.set_printoptions(threshold=sys.maxsize)
 # Create a robot object and initialize
 arlo = robot.Robot()
 
-try:
-    import picamera2
-    print("Camera.py: Using picamera2 module")
-except ImportError:
-    print("Camera.py: picamera2 module not available")
-    exit(-1)
-
-print("OpenCV version = " + cv2.__version__)
-
-xSize = 640
-ySize = 480
-#focal = 350
-focal = 1335.517241
-
-# Open a camera device for capturing
-imageSize = (xSize, ySize)
-FPS = 30
-cam = picamera2.Picamera2()
-frame_duration_limit = int(1/FPS * 1000000) # Microseconds
-# Change configuration to set resolution, framerate
-picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
-                                                            controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
-                                                            queue=False)
-cam.configure(picam2_config) # Not really necessary
-cam.start(show_preview=False)
-
-pprint(cam.camera_configuration()) # Print the camera configuration in use
-
-time.sleep(1)  # wait for camera to setup
-
 
 """ # Open a window
 WIN_RF = "Example 1"
@@ -57,18 +27,6 @@ def betterGoDiff(leftSpeed, rightSpeed, directionL, directionR, sleeptime):
    sleep(sleeptime-0.1)
 
 
-
-
-def drive30CM():
-    betterGoDiff(30, 35, 1, 1, 1.6)
-    # Wait a bit while robot moves forward
-   
-    
-    # send a stop command
-    print(arlo.stop())
-    
-    # Wait a bit before next command
-    sleep(0.5)
 
 def drive(distance):
     left_speed = 30
@@ -87,29 +45,6 @@ def drive(distance):
     print(arlo.stop())
 
     # Wait a bit before the next command
-    sleep(0.5)
-
-
-def turnLeft(degree):
-   sleep(0.041)
-   print(arlo.go_diff(64, 70, 0, 1))
-
-   sleep(0.0074 * degree + ((degree**2)*0.000013))
-   # send a stop command
-   print(arlo.stop())
-    
-   # Wait a bit before next command
-   sleep(0.5)
-   
-def turnRight(degree):
-    sleep(0.041)
-    print(arlo.go_diff(64, 70, 1, 0))
-    
-    sleep(0.0074 * degree + ((degree**2)*0.000013))
-    # send a stop command
-    print(arlo.stop())
-     
-    # Wait a bit before next command
     sleep(0.5)
 
 import rrt, robot_models
