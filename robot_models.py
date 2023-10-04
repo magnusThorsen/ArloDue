2db9c1e271bfe5f8dbd4dd8b9e91e63cbcb86179
KasperFrahm <gjr449@alumni.ku.dk>
2023-10-04 14:55:14 +0200

    Update robot_models.py

--- robot_models.py
+++ robot_models.py
@@ -63,8 +63,14 @@ class RobotModel:
             self.turnLeft(degrees) """
 
     def turnRobot(self, p1, p2):
-        ang1 = np.arctan2(*p1[::-1])
-        ang2 = np.arctan2(*p2[::-1])
+        ang1 = np.arctan2(p1[1], p1[0])
+        ang2 = np.arctan2(p2[1], p2[0])
+
+        print(p1)
+        print(p2)
+
+        print(ang1)
+        print(ang2)
 
         degrees = np.rad2deg((ang1 - ang2) % (2 * np.pi))
 
