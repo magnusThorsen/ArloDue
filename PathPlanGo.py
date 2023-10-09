4f1493bd6baa955bee13266324c93d9be8c7d27e
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-10-09 11:09:04 +0200

    Update PathPlanGo.py

--- PathPlanGo.py
+++ PathPlanGo.py
@@ -66,7 +66,6 @@ def pathPlanning():
     #path.insert(0, [0,-1])
     i = 1
     robo.turnRobo((0,-1),path[i], path[i+1])
-    i += 1
     while i < (len(path)-1):
         robo.turnRobo(path[i-1], path[i], path[i+1])
         distance = np.linalg.norm(np.subtract(path[i],path[i+1]))
