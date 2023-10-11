00f43dba2972c368a34f7371c54eadcf27e8c8e9
jakobdue <jakob.s.due@gmail.com>
2023-10-11 10:58:56 +0200

    Update selflocalize.py

--- python/selflocalize.py
+++ python/selflocalize.py
@@ -138,7 +138,7 @@ try:
 
 
     # Initialize particles
-    num_particles = 1
+    num_particles = 100
     particles = initialize_particles(num_particles)
 
     est_pose = particle.estimate_pose(particles) # The estimate of the robots current pose
