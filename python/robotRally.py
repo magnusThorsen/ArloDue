9390618ba9f96e0550583bd08d3d29930adc08d9
jakobdue <jakob.s.due@gmail.com>
2023-10-23 14:50:55 +0200

    Create robotRally.py

---
+++ python/robotRally.py
@@ -0,0 +1,141 @@
+import time
+from time import sleep
+from pprint import *
+import robot
+import sys
+import cv2 # Import the OpenCV library
+import cv2.aruco as aruco
+import numpy as np # Import Numpy library
+import detectLandmark3 as detectLandmark
+
+np.set_printoptions(threshold=sys.maxsize)
+
+# Create a robot object and initialize
+
+arlo = robot.Robot()
+
+try:
+    import picamera2
+    print("Camera.py: Using picamera2 module")
+except ImportError:
+    print("Camera.py: picamera2 module not available")
+    exit(-1)
+
+print("OpenCV version = " + cv2.__version__)
+
+xSize = 640
+ySize = 480
+#focal = 350
+focal = 1335.517241
+
+# Open a camera device for capturing
+imageSize = (xSize, ySize)
+FPS = 30
+cam = picamera2.Picamera2()
+frame_duration_limit = int(1/FPS * 1000000) # Microseconds
+# Change configuration to set resolution, framerate
+picam2_config = cam.create_video_configuration({"size": imageSize, "format": 'RGB888'},
+                                                            controls={"FrameDurationLimits": (frame_duration_limit, frame_duration_limit)},
+                                                            queue=False)
+cam.configure(picam2_config) # Not really necessary
+cam.start(show_preview=False)
+
+pprint(cam.camera_configuration()) # Print the camera configuration in use
+
+time.sleep(1)  # wait for camera to setup
+
+
+# Some color constants in BGR format
+CRED = (0, 0, 255)
+CGREEN = (0, 255, 0)
+CBLUE = (255, 0, 0)
+CCYAN = (255, 255, 0)
+CYELLOW = (0, 255, 255)
+CMAGENTA = (255, 0, 255)
+CWHITE = (255, 255, 255)
+CBLACK = (0, 0, 0)
+
+# Landmarks.
+# The robot knows the position of 2 landmarks. Their coordinates are in the unit centimeters [cm].
+landmarkIDs = [1, 2, 3, 4, 1]
+landmarks = {
+    1: (0.0, 0.0),  # Coordinates for landmark 1
+    2: (0.0, 300.0),  # Coordinates for landmark 2
+    3: (400.0, 0.0),  # Coordinates for landmark 3
+    4: (400.0, 300.0)  # Coordinates for landmark 4
+}
+landmark_colors = [CRED, CGREEN, CBLUE, CYELLOW] # Colors used when drawing the landmarks
+
+def turnLeft(degree):
+   sleep(0.041)
+   print(arlo.go_diff(64, 68, 0, 1))
+
+   sleep(0.0074 * degree + ((degree**2)*0.000001))
+   # send a stop command
+   print(arlo.stop())
+    
+   # Wait a bit before next command
+   sleep(0.2)
+
+def searchAndshow(ImpID): 
+    detected = False
+    # Load the image
+    image = cam.capture_array("main")  # Load your image here
+
+    # Define the dictionary
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_6X6_250)
+
+    # Detect markers in the image
+    corners, ids, rejected = cv2.aruco.detectMarkers(image, dictionary)
+
+    cameraMatrix = np.array([[focal, 0, xSize/2],
+                            [0, focal, ySize/2],
+                            [0, 0, 1]])
+    
+   # Draw the detected markers on the image
+    if len(corners) > 0:
+        cv2.aruco.drawDetectedMarkers(image, corners, ids)
+        
+        # Estimate pose for each detected marker
+        rvecs, tvecs, _ = cv2.aruco.estimatePoseSingleMarkers(corners, 200, cameraMatrix, None)
+
+        # Iterate through the detected markers and print their IDs and pose information
+        for i in range(len(ids)):
+            marker_id = ids[i][0]
+            translation_vector = tvecs
+
+            # Calculate the Euclidean distance (norm) from the camera to the marker
+            distance = np.linalg.norm(translation_vector)
+
+            print(f"Detected Marker ID: {marker_id}")
+            print(f"Distance to Marker {marker_id}: {distance} units")
+        if marker_id == ImpID:
+            detected = True
+
+    # Display the image with detected markers
+    cv2.imshow("Detected Markers", image)
+    return detected
+
+
+
+
+def turnDetect(landmarkID):
+    counter = 0
+    while cv2.waitKey(4) == -1: # Wait for a key pressed event
+        # print go diff 
+        if not searchAndshow(landmarkID) or counter != 17: 
+            turnLeft(20)
+            sleep(0.3)
+            counter += 1
+        else: 
+            print(arlo.stop())
+
+        
+
+def main():
+    for i in landmarkIDs:
+        turnDetect(landmarkIDs[i])
+            
+        
+
+
