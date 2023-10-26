c2bd873e57195af8cdddd9bf378bb5a43caf75f8
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-10-26 14:55:50 +0200

    Update planA1.py

--- planA1.py
+++ planA1.py
@@ -7,6 +7,8 @@ import cv2 # Import the OpenCV library
 import cv2.aruco as aruco
 import numpy as np # Import Numpy library
 import selfLocalizeRightA1 as SL
+#import particle from subfolder 
+import python.particle as particle
 
 np.set_printoptions(threshold=sys.maxsize)
 
