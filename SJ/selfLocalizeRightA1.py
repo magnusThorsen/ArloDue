470afec93a205a301c209a99a7a6ee33d14f6dd9
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-10-26 15:20:03 +0200

    Update selfLocalizeRightA1.py

--- SJ/selfLocalizeRightA1.py
+++ SJ/selfLocalizeRightA1.py
@@ -21,17 +21,17 @@ def isRunningOnArlo():
     return onRobot
 
 
-if isRunningOnArlo():
+""" if isRunningOnArlo():
     # XXX: You need to change this path to point to where your robot.py file is located
     sys.path.append("../../../../Arlo/python")
+ """
 
-
-try:
+""" try:
     import robot
     onRobot = True
 except ImportError:
     print("selflocalize.py: robot module not present - forcing not running on Arlo!")
-    onRobot = False
+    onRobot = False """
 
 
 
