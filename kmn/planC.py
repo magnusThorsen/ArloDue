a9eca1d26d85cb3bd64bcc16ceafad1fafae192d
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-10-27 15:17:03 +0200

    d

--- kmn/planC.py
+++ kmn/planC.py
@@ -5,7 +5,7 @@ import sys
 import cv2 # Import the OpenCV library
 import cv2.aruco as aruco
 import numpy as np # Import Numpy library
-import mknRallySelf as rs
+import mknRallySelf2 as rs
 
 np.set_printoptions(threshold=sys.maxsize)
 
