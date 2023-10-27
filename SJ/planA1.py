19d4f4f3bdd4dd10752b9ead9829ee044b0cf2db
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-10-27 14:26:32 +0200

    Update planA1.py

--- SJ/planA1.py
+++ SJ/planA1.py
@@ -127,7 +127,8 @@ def driveWithTime(distance):
     end_time = start_time + timeDrive
     left_speed = 31
     right_speed = 40
-    print(arlo.go_diff(0, 60, 1, 1))
+    print(arlo.go_diff(0, 80, 1, 1))
+    sleep(0.1)
     while time.time() < end_time:
         frontSensor = arlo.read_front_ping_sensor()
         rightSensor = arlo.read_right_ping_sensor()
