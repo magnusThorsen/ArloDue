6018f0695bf7142b1d6006230677df723dc66eb9
SimonKA12 <simon.krogh.anderson@gmail.com>
2023-11-06 12:52:02 +0100

    s

--- Rally/final_planA2_drive_update_final.py
+++ Rally/final_planA2_drive_update_final.py
@@ -557,7 +557,7 @@ def main():
                     particles = turnRight(40, particles)
 
                 # Self localize and create a path to the landmark
-            elif landmark < 2 :
+            elif num_landmark < 2 :
             
                 print("Main: didn't find the landmark")
                 particles, visitedObstacles = reposition_dum(visitedObstacles, particles, world, WIN_RF1, WIN_World)
